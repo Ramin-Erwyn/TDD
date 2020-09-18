7496217aab269f3c57c19bafcee483627fc4107f
RAMIN <r.erwyn@gmail.com>
2020-09-18 15:12:58 +0200

    troisieme commit

--- src/test/java/fr/p10/miage/rps/model/RockPaperScissorsTest.java
+++ src/test/java/fr/p10/miage/rps/model/RockPaperScissorsTest.java
@@ -2,12 +2,8 @@ package fr.p10.miage.rps.model;
 
 import static org.testng.Assert.*;
 
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Parameters;
-import org.testng.annotations.Test;
+import org.testng.annotations.*;
 
-import java.awt.print.Paper;
 import static fr.p10.miage.rps.model.Play.ROCK;
 import static fr.p10.miage.rps.model.Play.PAPER;
 import static fr.p10.miage.rps.model.Play.SCISSORS;
@@ -23,7 +19,7 @@ public void setUp(){
 public void tearDown(){
        rps = null;
    }
-   @Parameters({"PAPER","ROCK"})
+   @Parameters({"papier","pierre"})
 
    @Test
    public void testLost1(){
@@ -44,5 +40,38 @@ public void tearDown(){
        // -ea -Dtestng.dtd.http=true
       assertEquals(rps.play(Play.valueOf(p1),Play.valueOf(p2)), Resultat.WIN);
     }
+    @Parameters({"pierre","pierre"})
+    @Test
+    public void testWinPlay(String p1, String p2) {
+        assertEquals(rps.play(Play.valueOf(p1), Play.valueOf(p2)), Resultat.WIN);
+    }
+
+    @Parameters({"papier","papier"})
+    @Test
+    public void testTiePlay(String p1,String p2) {
+        assertEquals(rps.play(Play.valueOf(p1),Play.valueOf(p2)),Resultat.TIE);
+    }
+
+    @Parameters({"papier","ciseau"})
+    @Test
+    public void testLostPlay(String p1, String p2){
+        assertEquals(rps.play(Play.valueOf(p1),Play.valueOf(p2)),Resultat.LOST);
+    }
+
+    @Test ( dataProvider = "winData ")
+    Object[][] testWinPlay ( Play p1 , Play p2 ) {
+        Object[][] createWinData;
+        return new Object[][]{{ p1}, {p2}};}
+
+
+    @Test ( dataProvider = "TieData ")
+    Object[][] testTiePlay ( Play p1 , Play p2 ){
+        Object[ ][ ] createTieData;
+        return new Object[][]{{ p1}, {p2}};}
+
+    @Test ( dataProvider = "LostData ")
+    Object[][] testLostPlay ( Play p1 , Play p2 ){
+        Object [ ][ ] createLostData;
+        return new Object[][]{{ p1}, {p2}};}
 
 }
